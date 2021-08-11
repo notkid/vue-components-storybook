088d0ea46ae6a4a74020fb0e02eb30e9e6e31676
notkid <liuhaiyang1992@hotmail.com>
2021-08-11 08:20:36 +0800

    portal

---
+++ src/components/avatar/Avatar.stories.js
@@ -0,0 +1,39 @@
+import Avatar from './Avatar.vue';
+
+export default {
+  title: 'Example/Avatar',
+  component: Avatar,
+//   argTypes: {
+//     backgroundColor: { control: 'color' },
+//     size: { control: { type: 'select', options: ['small', 'medium', 'large'] } },
+//   },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Avatar },
+  template: '<Avatar>11</Avatar>',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
